fff5324ff2c64a7c3d6f8284d8d3a58be58d5aee
Natalia Istomina <ist0mNS@gmail.com>
2019-12-03 09:50:15 +0500

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import AdkuSidebar from 'adku-sidebar';
 
-import ExampleComponent from 'adku-sidebar'
+const SidebarContent = () => (
+  <div className="container">
+    <div className="d-flex flex-row">       
+      <button className="btn btn-primary">Закрыть</button>      
+    </div>
+  </div>
+);
 
 export default class App extends Component {
+  state = { show: false };
+
+  onOpen = () => {
+    this.setState({ show: true });
+  }
+
   render () {
+    const { show } = this.state;
+
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <AdkuSidebar className="border-right bg-secondary" show={show} sidebar={<SidebarContent/>}>
+          <div className="container">
+            <div className="row m-3"> 
+              <div className="col">
+                <button className="btn btn-primary" onClick={this.onOpen}>Показать</button>
+              </div>
+            </div>
+          </div>
+        </AdkuSidebar>
       </div>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,26 @@ import * as React from 'react'
 
 import styles from './styles.css'
 
-export type Props = { text: string }
+interface IProps {
+  className?: string;
+  show?: boolean;
+  children: React.ReactChildren,
+  sidebar: React.ReactNode
+};
 
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const {
-      text
-    } = this.props
+export default class AdkuSidebar extends React.Component<IProps> {  
+
+  render() {    
+    const { className, children, show, sidebar } = this.props;    
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div>        
+        <div className={`${styles.sidebar} ${styles.tran} ${className ? className : ''} ${show ? '' :  styles['sidebar-hidden']}`}>
+          { sidebar }
+        </div>
+        <div className={`${styles.tran}`}>
+          { children }
+        </div>
       </div>
     )
   }
